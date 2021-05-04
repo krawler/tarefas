022e9fdeace09089361f9125011f188a5a28040a
krawler <raframos@deloitte.com>
2021-05-04 18:10:50 -0300

    inicial

---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="tasks">
+    <template v-if="tasks.length">
+        <Task  v-for="task in tasks" :key="task.name" :task="task">
+        </Task>
+    </template>
+    <p v-else class="no-task">Sua vida está em dia</p>
+  </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+
+export default {
+  components: { Task },
+    props: {
+        tasks: { type: Array, required: true }
+    }
+}
+</script>
+
+<style>
+    .tasks{
+        display:flex;
+        justify-content: center;
+        flex-wrap:wrap;
+    }
+
+    .tasks .task{
+        margin: 10px;
+    }
+</style>
